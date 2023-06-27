950a26001e57359f6ef3dba89423c2ffe673d8fb
Kelly Anderson <69013811+anderske-msu@users.noreply.github.com>
2023-06-27 00:33:32 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='SympNetTorch',
+    version='0.0.1',
+    author='Kelly Anderson',
+    author_email='anderske@frib.msu.edu',
+    description='Sympletic PyTorch Modules',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anderske-msu/Sympletic-PyTorch',
+    project_urls = {
+        "Bug Tracker": "https://github.com/anderske-msu/Sympletic-PyTorch/issues"
+    },
+    packages=['SympNetTorch'],
+    install_requires=['torch'],
+)
