432d99fa4bb470ab1baaa35a0c99c359ee48d672
Kelly Anderson <anderske@frib.msu.edu>
2023-06-27 11:15:05 -0400

    Added activation module

--- SympNets.py
+++ SympNets.py
@@ -1,8 +1,15 @@
+from typing import Callable
 import torch
 import torch.nn as nn
 
+# TODO Add support for changing device
+
+# * Functions
+
 
 def check_device(device: torch.device) -> torch.device:
+    """Checks if device is a valid torch.device or is None. If None it will return a cpu device."""
+
     if device is None:
         return_device = torch.device("cpu")
     elif type(device) is torch.device:
@@ -15,6 +22,8 @@ def check_device(device: torch.device) -> torch.device:
 
 
 def check_up_or_low(up_or_low: str) -> str:
+    """Checks if a valid string was given for up_or_low."""
+
     if not up_or_low in ["up", "low"]:
         msg = f'Expected up_or_low to be "up" or "low" got "{up_or_low}".'
 
@@ -23,47 +32,203 @@ def check_up_or_low(up_or_low: str) -> str:
     return up_or_low
 
 
-class linear_sub(nn.Module):
+# * Sub Modules
+
+
+class activation_sub_up(nn.Module):
     def __init__(
-        self, dim: int = 2, up_or_low: str = "up", device: torch.device = None
+        self, func: Callable, dim: int = 2, device: torch.device = None
     ) -> None:
+        """Creates a upper trangular activation sympletic module.
+
+        func is the activation function to be applied. Should apply a nonlinear activation function element by element.
+        """
+
         super().__init__()
-        self.A = nn.Parameter(torch.randn((dim, dim)))  # S = A + A^T
-        self.eye = torch.eye(dim, dtype=torch.float32)
-        self.dim = dim
-        self.up_or_low = check_up_or_low(up_or_low)
+        self.a = nn.Parameter(torch.randn(dim))
         self.device = check_device(device)
+        self.dim = dim
+        self.func = func
 
     def forward(self, pq: torch.Tensor) -> torch.Tensor:
         pq_size = pq.size()
         npq = torch.empty_like(pq)
 
         if len(pq_size) == 2:
-            term_size = (pq_size[0], pq_size[1] // 2)
+            # This means the batch size is greater than 1 and will loop over the batch to get the term.
+            term_size = pq_size[0], pq_size[1] // 2
 
-            pterm = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
-            qterm = torch.zeros_like(pterm).to(self.device)
+            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
 
             for i in range(pq_size[0]):
-                if self.up_or_low == "up":
-                    pterm[i] = torch.mv(self.A + self.A.T, pq[i, self.dim :])
-
-                elif self.up_or_low == "low":
-                    qterm[i] = torch.mv(self.A + self.A.T, pq[i, : self.dim])
+                term[i] = torch.mv(
+                    torch.diag(self.a), self.func(pq[i, self.dim :])
+                )  # acting on q
 
         else:
-            if self.up_or_low == "up":
-                pterm = torch.mv(self.A + self.A.T, pq[self.dim :])
-                qterm = 0
+            term = torch.mv(
+                torch.diag(self.a), self.func(pq[self.dim :])
+            )  # acting on q
 
-            elif self.up_or_low == "low":
-                pterm = 0
-                qterm = torch.mv(self.A + self.A.T, pq[: self.dim])
-
-        npq[..., : self.dim] = pq[..., : self.dim] + pterm
-        npq[..., self.dim :] = pq[..., self.dim :] + qterm
+        npq[..., : self.dim] = pq[..., : self.dim] + term  # new p
+        npq[..., self.dim :] = pq[..., self.dim :]  # new q
 
         return npq
+
+
+class activation_sub_low(nn.Module):
+    def __init__(
+        self, func: Callable, dim: int = 2, device: torch.device = None
+    ) -> None:
+        """Creates a lower trangular activation sympletic module.
+
+        func is the activation function to be applied. Should apply a nonlinear activation function element by element.
+        """
+
+        super().__init__()
+        self.a = nn.Parameter(torch.randn(dim))
+        self.device = check_device(device)
+        self.dim = dim
+        self.func = func
+
+    def forward(self, pq: torch.Tensor) -> torch.Tensor:
+        pq_size = pq.size()
+        npq = torch.empty_like(pq)
+
+        if len(pq_size) == 2:
+            # This means the batch size is greater than 1 and will loop over the batch to get the term.
+            term_size = pq_size[0], pq_size[1] // 2
+
+            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
+
+            for i in range(pq_size[0]):
+                term[i] = torch.mv(
+                    torch.diag(self.a), self.func(pq[i, : self.dim])
+                )  # acting on p
+
+        else:
+            term = torch.mv(
+                torch.diag(self.a), self.func(pq[: self.dim])
+            )  # acting on p
+
+        npq[..., : self.dim] = pq[..., : self.dim]  # new p
+        npq[..., self.dim :] = pq[..., self.dim :] + term  # new q
+
+        return npq
+
+
+class linear_sub_low(nn.Module):
+    def __init__(self, dim: int = 2, device: torch.device = None) -> None:
+        """Creates a lower trangular linear sympletic module."""
+
+        super().__init__()
+        self.A = nn.Parameter(torch.randn((dim, dim)))  # S = A + A^T
+        self.device = check_device(device)
+        self.dim = dim
+
+    def forward(self, pq: torch.Tensor) -> torch.Tensor:
+        pq_size = pq.size()
+        npq = torch.empty_like(pq)
+
+        if len(pq_size) == 2:
+            # This means the batch size is greater than 1 and will loop over the batch to get the term.
+            term_size = pq_size[0], pq_size[1] // 2
+
+            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
+
+            for i in range(pq_size[0]):
+                term[i] = torch.mv(
+                    self.A + self.A.T, pq[i, : self.dim]
+                )  # Sp = (A +  A^T)p
+
+        else:
+            term = torch.mv(self.A + self.A.T, pq[: self.dim])  # Sp = (A +  A^T)p
+
+        npq[..., : self.dim] = pq[..., : self.dim]  # new p
+        npq[..., self.dim :] = pq[..., self.dim :] + term  # new q
+
+        return npq
+
+
+class linear_sub_up(nn.Module):
+    def __init__(self, dim: int = 2, device: torch.device = None) -> None:
+        """Creates an upper trangular linear sympletic module."""
+
+        super().__init__()
+        self.A = nn.Parameter(torch.randn((dim, dim)))  # S = A + A^T
+        self.device = check_device(device)
+        self.dim = dim
+
+    def forward(self, pq: torch.Tensor) -> torch.Tensor:
+        pq_size = pq.size()
+        npq = torch.empty_like(pq)
+
+        if len(pq_size) == 2:
+            # This means the batch size is greater than 1 and will loop over the batch to get the term.
+            term_size = pq_size[0], pq_size[1] // 2
+
+            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
+
+            for i in range(pq_size[0]):
+                term[i] = torch.mv(
+                    self.A + self.A.T, pq[i, self.dim :]
+                )  # Sq = (A +  A^T)q
+
+        else:
+            term = torch.mv(self.A + self.A.T, pq[self.dim :])  # Sq = (A +  A^T)q
+
+        npq[..., : self.dim] = pq[..., : self.dim] + term  # new p
+        npq[..., self.dim :] = pq[..., self.dim :]  # new q
+
+        return npq
+
+
+# * Full Modules
+
+
+class Activation(nn.Module):
+    def __init__(
+        self,
+        func: Callable,
+        dim: int = 2,
+        up_or_low: str = "up",
+        b: torch.Tensor = None,
+        device: torch.device = None,
+    ) -> None:
+        """Creates an activation sympmetic modules."""
+
+        super().__init__()
+        self.up_or_low = check_up_or_low(up_or_low)
+        self.device = check_device(device)
+        self.dim = dim
+
+        if up_or_low == "up":
+            self.layer = activation_sub_up(func, dim=dim, device=device)
+
+        elif up_or_low == "low":
+            self.layer = activation_sub_low(func, dim=dim, device=device)
+
+        if b is None:
+            self.b = torch.zeros(2 * dim, dtype=torch.float32).to(device)
+
+        else:
+            self.b = b.to(device)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pq = torch.empty_like(x)
+        nx = torch.empty_like(x)
+
+        pq[..., : self.dim] = x[..., 1::2].clone()
+        pq[..., self.dim :] = x[..., 0::2].clone()
+
+        pq = self.layer(pq)
+
+        pq += self.b
+
+        nx[..., : self.dim] = pq[..., 1::2].clone()
+        nx[..., self.dim :] = pq[..., 0::2].clone()
+
+        return nx
 
 
 class Linear(nn.Module):
@@ -75,8 +240,9 @@ class Linear(nn.Module):
         b: torch.Tensor = None,
         device: torch.device = None,
     ) -> None:
-        super().__init__()
+        """Creates an series of linear sympmetic modules."""
 
+        super().__init__()
         self.up_or_low = check_up_or_low(up_or_low)
         self.device = check_device(device)
         self.dim = dim
@@ -85,21 +251,21 @@ class Linear(nn.Module):
         mlist = []
 
         for _ in range(n):
-            mlist.append(linear_sub(dim=dim, up_or_low=uplow, device=device))
-
             if uplow == "up":
+                mlist.append(linear_sub_up(dim=dim, device=device))
                 uplow = "low"
 
             elif uplow == "low":
+                mlist.append(linear_sub_low(dim=dim, device=device))
                 uplow = "up"
 
         self.layers = nn.ModuleList(mlist)
 
         if b is None:
-            self.b = torch.zeros(2 * dim, dtype=torch.float32).to(self.device)
+            self.b = torch.zeros(2 * dim, dtype=torch.float32).to(device)
 
         else:
-            self.b = b.to(self.device)
+            self.b = b.to(device)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         pq = torch.empty_like(x)
