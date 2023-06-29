9cad87ae3c873d9c8a82c1a6e26d27e03c7b8b4b
Kelly Anderson <anderske@frib.msu.edu>
2023-06-29 15:46:32 -0400

    Added sklearn to installs for testing network Formatted

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='SympNetTorch',
-    version='0.0.1',
-    author='Kelly Anderson',
-    author_email='anderske@frib.msu.edu',
-    description='Sympletic PyTorch Modules',
+    name="SympNetTorch",
+    version="0.0.1",
+    author="Kelly Anderson",
+    author_email="anderske@frib.msu.edu",
+    description="Sympletic PyTorch Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/anderske-msu/SympNetsTorch',
-    project_urls = {
+    url="https://github.com/anderske-msu/SympNetsTorch",
+    project_urls={
         "Bug Tracker": "https://github.com/anderske-msu/SympNetsTorch/issues"
     },
-    packages=['SympNetTorch'],
-    install_requires=['torch'],
+    packages=["SympNetTorch"],
+    install_requires=["torch", "sklearn"],
 )
