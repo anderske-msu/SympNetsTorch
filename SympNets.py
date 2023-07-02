9e729e9d562df0946dc0c711c0ede81b56c57f17
Kelly Anderson <anderske@frib.msu.edu>
2023-07-02 15:27:42 -0400

    Added functions to reduce redundant code added module test

--- SympNets.py
+++ SympNets.py
@@ -1,6 +1,9 @@
 from typing import Callable
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
 
 # TODO Add support for changing device
 
@@ -45,6 +48,62 @@ def check_up_or_low(up_or_low: str) -> str:
     return up_or_low
 
 
+def mul_m_pq(mat: torch.Tensor, pq: torch.Tensor) -> torch.Tensor:
+    pq_size = pq.size()
+
+    if len(pq_size) == 2:
+        # This is a batch
+        with torch.no_grad():
+            mat = torch.stack(tuple(mat for _ in range(pq_size[0])))
+        pq = torch.bmm(mat, pq.reshape(*pq_size, 1)).reshape(pq_size)
+
+    else:
+        pq = torch.mv(mat, pq)
+
+    return pq
+
+
+def activate_pq_term(
+    func: Callable,
+    a: torch.Tensor,
+    pq: torch.Tensor,
+    term_index: tuple,
+    pq_index: tuple,
+) -> torch.Tensor:
+    pq_size = pq.size()
+    tstart, tend = term_index
+    pqstart, pqend = pq_index
+    term = torch.zeros_like(pq)
+
+    if len(pq_size) == 2:
+        # This means the batch size is greater than 1 and will loop over the batch to get the term.
+
+        for i in range(pq_size[0]):
+            term[i, tstart:tend] = torch.mv(torch.diag(a), func(pq[i, pqstart:pqend]))
+
+    else:
+        term[tstart:tend] = torch.mv(torch.diag(a), func(pq[pqstart:pqend]))
+
+    return term
+
+
+def linear_matrix(
+    A: torch.Tensor,
+    dim: int,
+    index_1: tuple,
+    index_2: tuple,
+    dtype: type,
+    device: torch.device,
+) -> torch.Tensor:
+    s1, e1 = index_1
+    s2, e2 = index_2
+
+    m = torch.eye(2 * dim, dtype=dtype, device=device)
+    m[s1:e1, s2:e2] = A + A.T
+
+    return m
+
+
 # * Sub Modules
 
 
@@ -64,22 +123,9 @@ class activation_sub_up(nn.Module):
         self.func = func
 
     def forward(self, pq: torch.Tensor) -> torch.Tensor:
-        pq_size = pq.size()
-
-        if len(pq_size) == 2:
-            # This means the batch size is greater than 1 and will loop over the batch to get the term.
-            # Acting on q
-            term_size = pq_size[0], pq_size[1] // 2
-
-            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
-
-            for i in range(pq_size[0]):
-                term[i] = torch.mv(torch.diag(self.a), self.func(pq[i, self.dim :]))
-
-        else:
-            term = torch.mv(torch.diag(self.a), self.func(pq[self.dim :]))
-
-        pq[..., : self.dim] += term  # new p
+        pq += activate_pq_term(
+            self.func, self.a, pq, term_index=(0, self.dim), pq_index=(self.dim, None)
+        )  # Acts on q, gives new p
 
         return pq
 
@@ -100,22 +146,9 @@ class activation_sub_low(nn.Module):
         self.func = func
 
     def forward(self, pq: torch.Tensor) -> torch.Tensor:
-        pq_size = pq.size()
-
-        if len(pq_size) == 2:
-            # This means the batch size is greater than 1 and will loop over the batch to get the term.
-            # Acting on p
-            term_size = pq_size[0], pq_size[1] // 2
-
-            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
-
-            for i in range(pq_size[0]):
-                term[i] = torch.mv(torch.diag(self.a), self.func(pq[i, : self.dim]))
-
-        else:
-            term = torch.mv(torch.diag(self.a), self.func(pq[: self.dim]))
-
-        pq[..., self.dim :] += term  # new q
+        pq += activate_pq_term(
+            self.func, self.a, pq, term_index=(self.dim, None), pq_index=(0, self.dim)
+        )  # Acts on p, gives new q
 
         return pq
 
@@ -130,22 +163,15 @@ class linear_sub_low(nn.Module):
         self.dim = dim
 
     def forward(self, pq: torch.Tensor) -> torch.Tensor:
-        pq_size = pq.size()
-
-        if len(pq_size) == 2:
-            # This means the batch size is greater than 1 and will loop over the batch to get the term.
-            # Sp = (A +  A^T)p
-            term_size = pq_size[0], pq_size[1] // 2
-
-            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
-
-            for i in range(pq_size[0]):
-                term[i] = torch.mv(self.A + self.A.T, pq[i, : self.dim])
-
-        else:
-            term = torch.mv(self.A + self.A.T, pq[: self.dim])
-
-        pq[..., self.dim :] += term  # new q
+        matmul = linear_matrix(
+            self.A,
+            self.dim,
+            index_1=(self.dim, None),
+            index_2=(0, self.dim),
+            dtype=pq.dtype,
+            device=self.device,
+        )
+        pq = mul_m_pq(matmul, pq)
 
         return pq
 
@@ -160,22 +186,15 @@ class linear_sub_up(nn.Module):
         self.dim = dim
 
     def forward(self, pq: torch.Tensor) -> torch.Tensor:
-        pq_size = pq.size()
-
-        if len(pq_size) == 2:
-            # This means the batch size is greater than 1 and will loop over the batch to get the term.
-            # Sq = (A +  A^T)q
-            term_size = pq_size[0], pq_size[1] // 2
-
-            term = torch.zeros(term_size, dtype=pq.dtype).to(self.device)
-
-            for i in range(pq_size[0]):
-                term[i] = torch.mv(self.A + self.A.T, pq[i, self.dim :])
-
-        else:
-            term = torch.mv(self.A + self.A.T, pq[self.dim :])
-
-        pq[..., : self.dim] += term  # new p
+        matmul = linear_matrix(
+            self.A,
+            self.dim,
+            index_1=(0, self.dim),
+            index_2=(self.dim, None),
+            dtype=pq.dtype,
+            device=self.device,
+        )
+        pq = mul_m_pq(matmul, pq)
 
         return pq
 
@@ -244,7 +263,7 @@ class Linear(nn.Module):
         self.layers = nn.ModuleList(mlist)
 
         if b is None:
-            self.b = torch.zeros(2 * dim, dtype=torch.float32).to(self.device)
+            self.b = torch.zeros(2 * dim, dtype=torch.float32, device=self.device)
 
         else:
             self.b = b.to(self.device)
@@ -260,3 +279,153 @@ class Linear(nn.Module):
         nx = x_to_pq(pq)
 
         return nx
+
+
+class test_network(nn.Module):
+    def __init__(self, device: torch.device = None) -> None:
+        super().__init__()
+        self.device = check_device(device)
+        dim = 2
+
+        self.layers = nn.ModuleList(
+            [
+                Linear(
+                    dim=dim,
+                    up_or_low="up",
+                    n=4,
+                    device=self.device,
+                    b=torch.ones(2 * dim),
+                ),
+                Activation(torch.tanh, dim=dim, up_or_low="up", device=self.device),
+                Linear(
+                    dim=dim,
+                    up_or_low="low",
+                    n=4,
+                    b=torch.ones(2 * dim),
+                    device=self.device,
+                ),
+                Activation(torch.tanh, dim=dim, up_or_low="low", device=self.device),
+            ]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        torch.autograd.set_detect_anomaly(True)
+        pq = x_to_pq(x)
+
+        for layer in self.layers:
+            pq = layer(pq)
+
+        nx = x_to_pq(pq)
+
+        return nx
+
+
+def test_modules(
+    numdata: int = 1000, batch_size: int = 150, device: torch.device = None
+):
+    print("Starting test...")
+    success = False
+    # Split info
+    test_size = 0.3
+    random_state = 3
+
+    if device is None:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    nn_model = test_network(device).to(device)
+
+    def train_model(model, epochs, train_loader):
+        train_losses = []
+        train_counter = []
+
+        # set network to training mode
+        model.train()
+
+        # iterate through data batches
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # reset gradients
+            optimizer.zero_grad()
+
+            # evaluate network with data
+            output = model(data)
+
+            # compute loss and derivative
+            loss = F.mse_loss(output, target)
+            loss.backward()
+
+            # step optimizer
+            optimizer.step()
+
+            # print out results and save to file
+            train_losses.append(loss.item())
+            train_counter.append(
+                (batch_idx * batch_size) + ((epochs - 1) * len(train_loader.dataset))
+            )
+
+        return train_losses, train_counter
+
+    def test_model(model, test_loader):
+        test_losses = []
+
+        model.eval()
+
+        test_loss = 0
+
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = model(data)
+
+                test_loss += F.mse_loss(output, target).item()
+
+        test_loss /= len(test_loader.dataset)
+        test_losses.append(test_loss)
+
+        return test_losses
+
+    # Main
+
+    train_x, test_x, train_y, test_y = train_test_split(
+        torch.randn((numdata, 4)),
+        torch.randn((numdata, 4)),
+        test_size=test_size,
+        random_state=random_state,
+    )
+
+    train_x = train_x.to(device)
+    test_x = test_x.to(device)
+    train_y = train_y.to(device)
+    test_y = test_y.to(device)
+
+    # Network and Constraint Objects
+    optimizer = optim.Adam(nn_model.parameters())
+
+    train_data = torch.utils.data.TensorDataset(train_x, train_y)
+
+    test_data = torch.utils.data.TensorDataset(test_x, test_y)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_data, batch_size=batch_size, shuffle=True
+    )
+
+    train_losses = []
+    test_losses = []
+    train_count = []
+
+    n_epochs = 5
+
+    for epoch in range(1, n_epochs + 1):
+        temp = train_model(nn_model, epoch, train_loader)
+
+        train_losses += temp[0]
+        train_count += temp[1]
+
+        test_losses += test_model(nn_model, test_loader)
+
+    success = True
+
+    print("Test Done")
+
+    return success
